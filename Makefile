5e60de61120903c7c6ff85c6cdb6a0c610eeaf7a
Magnus Hagdorn <Magnus.Hagdorn@ed.ac.uk>
2005-02-03 11:21:32 +0000

    initial version
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/glimmer-cism/glimmer-example/trunk@286 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

---
+++ Makefile
@@ -0,0 +1,20 @@
+# Makefile for building the example climate driver
+# Magnus Hagdorn, November 2004
+
+# setup compilation flags: tell the compiler where to look for f90 module files
+GLIMMER_FFLAGS = -I$(GLIMMER_PREFIX)/include
+# setup linker flags: tell the linker where to look for libraries and which libraries to link
+GLIMMER_LIBRARIES = -L$(GLIMMER_PREFIX)/lib -lglide -lglimmer -lslap -lnetcdf
+
+# set variables FCFLAGS and LDFLAGS for extra libraries, e.g. netCDF
+
+# how to build the example executable
+example:	example.o
+		$(FC) -o $@ $< $(LDFLAGS) $(GLIMMER_LIBRARIES)
+
+# implicit rule, telling the make system how to build an object file from a .f90 file
+.f90.o:
+		$(FC) -fpp $(FCFLAGS) $(GLIMMER_FFLAGS) -c $< -o $@
+
+
+.SUFFIXES:	.f90 .o
---
+++ example.f90
@@ -0,0 +1,90 @@
+! ******************************************************************************
+! example.f90
+! Magnus Hagdorn
+!
+! simple example climate driver demonstrating how to use the library
+! ******************************************************************************
+!
+! ChangeLog
+! 2004-11-10 Magnus Hagdorn
+!  * initial version
+
+program example
+
+  ! load various modules
+  use glimmer_global, only:rk ! precision of the model
+  use glide                   ! main glide module
+  use glimmer_log             ! module for logging messages
+  use glimmer_config          ! module for handling configuration files
+  use paramets, only : len0   ! for the length scale
+  implicit none
+
+  ! some variables
+  type(glide_global_type) :: model    
+  ! this variable holds all the data associated with this particular model
+  ! instance.
+  type(ConfigSection), pointer :: config
+  ! this pointer points to the first element of a linked list which contains
+  ! all the configuration variables and their values
+  character(len=50) :: fname   
+  ! name of paramter file
+  real(kind=rk) time
+  ! current time
+  
+  integer ew,ns,ewct,nsct  ! loop variable and grid centre
+  real grid, dist          ! node spacing and distance
+
+  ! Ask for configuration file
+  write(*,*) 'Enter name of GLIDE configuration file to be read'
+  read(*,*) fname
+  
+  ! start logging
+  call open_log(unit=50)
+  
+  ! read configuration
+  call ConfigRead(fname,config)
+
+  ! initialise GLIDE
+  call glide_initialise(model,config)
+  ! fill dimension variables
+  call glide_nc_fillall(model)
+  ! get current time from start time
+  time = model%numerics%tstart
+
+  ! setup some variables for BC
+  ewct = real(model%general%ewn+1) / 2.0 ! the grid centre (x)
+  nsct = real(model%general%nsn+1) / 2.0 ! and (y)
+  grid = model%numerics%dew * len0       ! node-spacing
+
+  ! loop over times
+  do while(time.le.model%numerics%tend)
+     ! setup boundary conditions
+     ! this example implements the EISMINT-1 moving margin BC
+     
+     ! mass balance
+     ! loop over grid
+     do ns = 1,model%general%nsn
+        do ew = 1,model%general%ewn
+           ! calculate distance from centre
+           dist = grid * sqrt((real(ew) - ewct)**2 + (real(ns) - nsct)**2)
+           ! set mass balance
+           model%climate%acab(ew,ns) = min(0.5, 1.05e-5 * (450.0e3 - dist))
+        end do
+     end do
+     
+     ! surface temperature
+     model%climate%artm(:,:) = -3.150  -1.e-2 * model%geometry%thck(:,:)
+
+     ! calculate temperature and velocity distribution
+     call glide_tstep_p1(model,time)
+     ! write to netCDF file, move ice
+     call glide_tstep_p2(model)
+     ! calculate isostatic adjustment
+     call glide_tstep_p3(model)
+     ! increment time counter
+     time = time + model%numerics%tinc
+  end do
+
+  ! finalise GLIDE
+  call glide_finalise(model)
+end program example
